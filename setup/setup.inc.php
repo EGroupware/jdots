1346cecd981755d4915f166b020848790ee50c32
Ralf Becker <ralfbecker@outdoor-training.de>
2010-06-04 09:39:06 +0000

    setting Stylite GmbH as maintainer and Andreas+Ralf as author

--- setup/setup.inc.php
+++ setup/setup.inc.php
@@ -14,11 +14,14 @@ $GLOBALS['egw_info']['template']['jdots']['name']      = 'jdots';
 $GLOBALS['egw_info']['template']['jdots']['title']     = 'jDots';
 $GLOBALS['egw_info']['template']['jdots']['version']   = '1.0';
 
-$GLOBALS['egw_info']['template']['jdots']['author'] = 'Andreas Stoeckel';
+$GLOBALS['egw_info']['template']['jdots']['author'] = array(
+   array('name' => 'Andreas Stoeckel', 'email' => 'as@stylite.de'),
+   array('name' => 'Ralf Becker', 'email' => 'rb@stylite.de'),
+);
 $GLOBALS['egw_info']['template']['jdots']['license'] = 'EPL';
 $GLOBALS['egw_info']['template']['jdots']['icon'] = "jdots/images/jdots-logo.png";
 $GLOBALS['egw_info']['template']['jdots']['maintainer'] = array(
-   array('name' => 'Andreas Stoeckel', 'email' => 'as@stylite.de')
+   array('name' => 'Stylite GmbH', 'url' => 'http://www.stylite.de/')
 );
 $GLOBALS['egw_info']['template']['jdots']['description'] = "jDots is a new jQuery based template set for EGroupware.";
 $GLOBALS['egw_info']['template']['jdots']['windowed'] = true;
