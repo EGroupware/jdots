01f86ffd968c144e1a3dbb4fdd70cc92972c3354
Ralf Becker <ralfbecker@outdoor-training.de>
2010-06-08 20:49:34 +0000

    renamed jDots to Stylite

--- setup/setup.inc.php
+++ setup/setup.inc.php
@@ -11,7 +11,7 @@
  */
 
 $GLOBALS['egw_info']['template']['jdots']['name']      = 'jdots';
-$GLOBALS['egw_info']['template']['jdots']['title']     = 'jDots';
+$GLOBALS['egw_info']['template']['jdots']['title']     = 'Stylite';
 $GLOBALS['egw_info']['template']['jdots']['version']   = '1.0';
 
 $GLOBALS['egw_info']['template']['jdots']['author'] = array(
@@ -23,6 +23,6 @@ $GLOBALS['egw_info']['template']['jdots']['icon'] = "jdots/images/jdots-logo.png
 $GLOBALS['egw_info']['template']['jdots']['maintainer'] = array(
    array('name' => 'Stylite GmbH', 'url' => 'http://www.stylite.de/')
 );
-$GLOBALS['egw_info']['template']['jdots']['description'] = "jDots is a new jQuery based template set for EGroupware.";
+$GLOBALS['egw_info']['template']['jdots']['description'] = "Stylite is a new jQuery based template set for EGroupware EPL.";
 $GLOBALS['egw_info']['template']['jdots']['windowed'] = true;
 $GLOBALS['egw_info']['template']['jdots']['icon'] = 'jdots/images/epl.png';
