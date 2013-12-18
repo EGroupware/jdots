71b3d063acf90216b4661619579a5f6e2b55d489
Ralf Becker <ralfbecker@outdoor-training.de>
2013-12-18 05:20:52 +0000

    remove restriction that ajax=true must be last parameter on url

--- js/egw_fw.js
+++ js/egw_fw.js
@@ -1122,7 +1122,7 @@ egw_fw_content_browser.prototype.browse = function(_url)
 	// by calling the ajax_exec function
 	// we now send whole url back to server, so apps can use $_GET['ajax']==='true'
 	// to detect app-icon was clicked and eg. further reset filters
-	var matches = _url.match(/\/index.php\?menuaction=([A-Za-z0-9_\.]*.*&ajax=true)$/);
+	var matches = _url.match(/\/index.php\?menuaction=([A-Za-z0-9_\.]*.*&ajax=true.*)$/);
 	if (matches) {
 		// Matches[1] contains the menuaction which should be executed - replace
 		// the given url with the following line. This will be evaluated by the
